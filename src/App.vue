7007a9f5b07814ae7348b2832c0060ab2f493f6d
EmmanuelJmz <105473431+EmmanuelJmz@users.noreply.github.com>
2024-01-10 19:05:50 -0600

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <div>
+    <div>
+      <b-navbar tabs align="center" type="dark" variant="info">
+        <b-navbar-nav>
+          
+          <b-navbar-brand href="#">
+            <img src="https://placekitten.com/g/30/30" alt="Kitten">
+          </b-navbar-brand>
+    
+          <!-- Navbar dropdowns -->
+          <b-nav-item-dropdown text="Idioma" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+    
+          <b-nav-item-dropdown text="Usuario" right>
+            <b-dropdown-item href="#">Account</b-dropdown-item>
+            <b-dropdown-item href="#">Settings</b-dropdown-item>
+          </b-nav-item-dropdown>
+          
+        </b-navbar-nav>
+<template>
+  <div>
+    <b-button v-b-toggle.sidebar-1>Toggle Sidebar</b-button>
+    <b-sidebar id="sidebar-1" title="Sidebar" shadow >
+      <div class="px-3 py-2">
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+          in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+        </p>
+        <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
+      </div>
+    </b-sidebar>
+  </div>
+</template>
+      </b-navbar>
+      
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
